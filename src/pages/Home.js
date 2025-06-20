0b7fafd4d07c9c60a251e08508b7ff57f138a45c
ksp0814 <rkdtpvlf456@naver.com>
2025-06-20 17:01:58 +0900

    문의작성 폼 글자수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <div className='hero'>
         <img src={HeroImg} alt="히어로 이미지" className='hero-image' />
         <div className='hero-content'>
-          <h1>회사명</h1>
+          <h1>K-경일이엔지</h1>
           <p>우리와 함께 성장하세요</p>
         </div>
       </div>
