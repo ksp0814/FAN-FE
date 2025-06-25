6afcdffd2ed82652c6029d2978d9fbcfdd30957f
ksp0814 <rkdtpvlf456@naver.com>
2025-06-25 17:42:50 +0900

    수정

--- src/pages/About.js
+++ src/pages/About.js
@@ -49,14 +49,27 @@ const About = () => {
         
         <div className="location-content">
           <div className="map-container">
-            <div className="map-placeholder">
-              {/* 실제 구현시에는 Google Maps나 Kakao Map API 사용 */}
-              <img 
-                src="/images/map.png" 
-                alt="회사 위치 지도" 
-                className="map-image"
-              />
-            </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50625.77203539618!2d126.71545167832031!3d37.61509990000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b79a6b25c7b0f%3A0x5c7f0b9b0b0b0b0b!2z6rK96riw64-E6rmA7Y+s7IucIOq5gO2PrOyLnA!5e0!3m2!1sko!2skr!4v1234567890123!5m2!1sko!2skr"
+              width="100%"
+              height="350"
+              style={{ border: 0, borderRadius: '16px' }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="K-경일이엔지 위치"
+            ></iframe>
+            <a 
+              href="https://maps.google.com/maps?daddr=경기도 김포시" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="directions-btn-overlay"
+            >
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2L13.09 8.26L22 9L13.09 9.74L12 16L10.91 9.74L2 9L10.91 8.26L12 2Z" fill="currentColor"/>
+              </svg>
+              길찾기
+            </a>
           </div>
           
           <div className="contact-info">
